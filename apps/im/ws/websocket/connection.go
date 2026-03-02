da015f4d453547abf1824e0c39ca5b2efd75c6cc
onychen <2752845347@qq.com>
2026-03-02 18:21:29 +0800

    complete ack part

--- apps/im/ws/websocket/connection.go
+++ apps/im/ws/websocket/connection.go
@@ -1,33 +1,36 @@
 package websocket
 
 import (
-	"fmt"
+	"github.com/gorilla/websocket"
 	"net/http"
 	"sync"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 type Conn struct {
-	*websocket.Conn
-
-	s *Server
+	idleMu sync.Mutex
 
 	Uid string
 
-	idleMu            sync.Mutex // guard the following
+	*websocket.Conn
+	s *Server
+
 	idle              time.Time
 	maxConnectionIdle time.Duration
+
+	messageMu      sync.Mutex
+	readMessage    []*Message
+	readMessageSeq map[string]*Message
+
+	message chan *Message
 
 	done chan struct{}
 }
 
 func NewConn(s *Server, w http.ResponseWriter, r *http.Request) *Conn {
-
 	c, err := s.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		s.Error("upgrade http conn err", err)
+		s.Errorf("upgrade err %v", err)
 		return nil
 	}
 
@@ -35,17 +38,53 @@ func NewConn(s *Server, w http.ResponseWriter, r *http.Request) *Conn {
 		Conn:              c,
 		s:                 s,
 		idle:              time.Now(),
-		done:              make(chan struct{}),
 		maxConnectionIdle: s.opt.maxConnectionIdle,
+		readMessage:       make([]*Message, 0, 2),
+		readMessageSeq:    make(map[string]*Message, 2),
+		message:           make(chan *Message, 1),
+		done:              make(chan struct{}),
 	}
 
 	go conn.keepalive()
 	return conn
 }
 
+func (c *Conn) appendMsgMq(msg *Message) {
+	c.messageMu.Lock()
+	defer c.messageMu.Unlock()
+
+	// 读队列中
+	if m, ok := c.readMessageSeq[msg.Id]; ok {
+		// 已经有消息的记录，该消息已经有ack的确认
+		if len(c.readMessage) == 0 {
+			// 队列中没有该消息
+			return
+		}
+
+		// msg.AckSeq > m.AckSeq
+		if m.AckSeq >= msg.AckSeq {
+			// 没有进行ack的确认, 重复
+			return
+		}
+
+		c.readMessageSeq[msg.Id] = msg
+		return
+	}
+	// 还没有进行ack的确认, 避免客户端重复发送多余的ack消息
+	if msg.FrameType == FrameAck {
+		return
+	}
+
+	c.readMessage = append(c.readMessage, msg)
+	c.readMessageSeq[msg.Id] = msg
+
+}
+
 func (c *Conn) ReadMessage() (messageType int, p []byte, err error) {
-	// 开始忙碌
 	messageType, p, err = c.Conn.ReadMessage()
+
+	c.idleMu.Lock()
+	defer c.idleMu.Unlock()
 	c.idle = time.Time{}
 	return
 }
@@ -53,8 +92,8 @@ func (c *Conn) ReadMessage() (messageType int, p []byte, err error) {
 func (c *Conn) WriteMessage(messageType int, data []byte) error {
 	c.idleMu.Lock()
 	defer c.idleMu.Unlock()
+	// 方法是并不安全
 	err := c.Conn.WriteMessage(messageType, data)
-	// 当写操作完成后当前连接就会进入空闲状态，并记录空闲的时间
 	c.idle = time.Now()
 	return err
 }
@@ -63,32 +102,29 @@ func (c *Conn) Close() error {
 	select {
 	case <-c.done:
 	default:
-		return c.Conn.Close()
+		close(c.done)
 	}
 
 	return c.Conn.Close()
 }
 
-// 长连接检测机制(思路是grpc的keepalive)
 func (c *Conn) keepalive() {
 	idleTimer := time.NewTimer(c.maxConnectionIdle)
-	defer idleTimer.Stop()
+	defer func() {
+		idleTimer.Stop()
+	}()
 
 	for {
 		select {
 		case <-idleTimer.C:
 			c.idleMu.Lock()
-			defer c.idleMu.Unlock()
 			idle := c.idle
-
-			fmt.Printf("idle %v, maxIdle %v \n", c.idle, c.maxConnectionIdle)
 			if idle.IsZero() { // The connection is non-idle.
 				c.idleMu.Unlock()
 				idleTimer.Reset(c.maxConnectionIdle)
 				continue
 			}
 			val := c.maxConnectionIdle - time.Since(idle)
-			fmt.Printf("val %v \n", val)
 			c.idleMu.Unlock()
 			if val <= 0 {
 				// The connection has been idle for a duration of keepalive.MaxConnectionIdle or more.
@@ -98,7 +134,6 @@ func (c *Conn) keepalive() {
 			}
 			idleTimer.Reset(val)
 		case <-c.done:
-			fmt.Println("客户端结束连接")
 			return
 		}
 	}
