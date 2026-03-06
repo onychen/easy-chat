95904b65f7c75c5ee5e9370774ff90d3897e0d8f
onychen <2752845347@qq.com>
2026-03-06 22:16:03 +0800

    fix

--- apps/user/rpc/user.go
+++ apps/user/rpc/user.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"easy-chat/pkg/interceptor/rpcserver"
 	"flag"
 	"fmt"
+	"sync"
 
 	"easy-chat/apps/user/rpc/internal/config"
 	"easy-chat/apps/user/rpc/internal/server"
 	"easy-chat/apps/user/rpc/internal/svc"
 	"easy-chat/apps/user/rpc/user"
+	"easy-chat/pkg/configserver"
+	"easy-chat/pkg/interceptor/rpcserver"
 
-	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/proc"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
@@ -19,20 +21,59 @@ import (
 
 var configFile = flag.String("f", "etc/local/user.yaml", "the config file")
 
+var grpcServer *grpc.Server
+var wg sync.WaitGroup
+
 func main() {
 	flag.Parse()
 
 	var c config.Config
-	conf.MustLoad(*configFile, &c)
+	err := configserver.NewConfigServer(*configFile, configserver.NewSail(&configserver.Config{
+		ETCDEndpoints:  "192.168.100.1:3379",
+		ProjectKey:     "98c6f2c2287f4c73cea3d40ae7ec3ff2",
+		Namespace:      "user",
+		Configs:        "user-rpc.yaml",
+		ConfigFilePath: "./conf",
+		LogLevel:       "DEBUG",
+	})).MustLoad(&c, func(bytes []byte) error {
+		var c config.Config
+		configserver.LoadFromJsonBytes(bytes, &c)
+
+		proc.WrapUp()
+
+		wg.Add(1)
+		go func(c config.Config) {
+			defer wg.Done()
+
+			Run(c)
+		}(c)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	wg.Add(1)
+	go func(c config.Config) {
+		defer wg.Done()
+
+		Run(c)
+	}(c)
+
+	wg.Wait()
+}
+
+func Run(c config.Config) {
 	ctx := svc.NewServiceContext(c)
 
 	if err := ctx.SetRootToken(); err != nil {
 		panic(err)
 	}
 
-	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		user.RegisterUserServer(grpcServer, server.NewUserServer(ctx))
+	s := zrpc.MustNewServer(c.RpcServerConf, func(srv *grpc.Server) {
+		grpcServer = srv
 
+		user.RegisterUserServer(grpcServer, server.NewUserServer(ctx))
 		if c.Mode == service.DevMode || c.Mode == service.TestMode {
 			reflection.Register(grpcServer)
 		}
